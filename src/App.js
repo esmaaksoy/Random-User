0437d46d6bdc62d16c48c5cce62398fb2150c0e9
esma <esma.ea.aksoy@gmail.com>
2023-12-14 19:38:51 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from "react";
 import mailSvg from "./assets/mail.svg";
-import manSvg from "./assets/man.svg";
+
 import womanSvg from "./assets/woman.svg";
-import manAgeSvg from "./assets/growing-up-man.svg";
+
 import womanAgeSvg from "./assets/growing-up-woman.svg";
 import mapSvg from "./assets/map.svg";
 import phoneSvg from "./assets/phone.svg";
 import padlockSvg from "./assets/padlock.svg";
-import cwSvg from "./assets/cw.svg";
-import Footer from "./components/footer/Footer";
 import Table from "./components/Table";
 const url = "https://randomuser.me/api/";
-const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
 
 function App() {
-  const [addUser, setAddUser] = useState([])
-  const [show,setShow] =useState("")
-  const [user,setUser]=useState({
+  const [addUser, setAddUser] = useState([]);
+  const [labels, setLabels] = useState("name");
+  const [user, setUser] = useState({
     name: "",
     email: "",
     picture: "",
@@ -24,19 +22,17 @@ function App() {
     location: "",
     phone: "",
     gender: "",
-   
-  })
+  });
   const {
-    name: {first,last},
+    name: { first, last },
     email,
     dob: { age },
-    location: { street},
+    location: { street },
     phone,
     picture,
     login,
-   
-  } = user
-  const [data, setData] = useState(first);
+  } = user;
+  const [data, setData] = useState(`${first} ${last}`);
 
   const fetchUsers = async () => {
     try {
@@ -53,30 +49,27 @@ function App() {
     fetchUsers();
   }, []);
 
- 
+  const handleMouseEnter = (info, category) => {
+    setData(info);
+    setLabels(category);
+  };
 
-const [labels,setLabels]=useState("name")
-const handleMouseEnter = (info,category) => {
-  setData(info)
-setLabels(category)
-};
-
-
-const handleAdd =()=>{
-  
-  setAddUser([...addUser,{
-    firstname: first, 
-    email: email, 
-    phone: phone, 
-   age: age,
-  }]);
-
-}
-
+  const handleAdd = () => {
+    setAddUser([
+      ...addUser,
+      {
+        firstname: first,
+        email: email,
+        phone: phone,
+        age: age,
+      },
+    ]);
+  };
+console.log(last)
   return (
     <main>
       <div className="block bcg-orange">
-        <img src={cwSvg} alt="cw" id="cw" />
+
       </div>
       <div className="block">
         <div className="container">
@@ -84,27 +77,46 @@ const handleAdd =()=>{
           <p className="user-title">My {labels} is</p>
           <p className="user-value">{data}</p>
           <div className="values-list">
-            <button className="icon" data-label="name" onMouseEnter={() => handleMouseEnter(first,"name")}>
-              <img
-                src={womanSvg}
-                alt="user"
-                id="iconImg"
-               
-              />
+            <button
+              className="icon"
+              data-label="name"
+              onMouseEnter={() => handleMouseEnter(`${first} ${last}`, "name")}
+            >
+              <img src={womanSvg} alt="user" id="iconImg" />
             </button>
-            <button className="icon" data-label="email" onMouseEnter={() => handleMouseEnter(email,"email")}>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseEnter={() => handleMouseEnter(email, "email")}
+            >
               <img src={mailSvg} alt="mail" id="iconImg" />
             </button>
-            <button className="icon" data-label="age" onMouseEnter={() => handleMouseEnter(age,"age")}>
+            <button
+              className="icon"
+              data-label="age"
+              onMouseEnter={() => handleMouseEnter(age, "age")}
+            >
               <img src={womanAgeSvg} alt="age" id="iconImg" />
             </button>
-            <button className="icon" data-label="street" onMouseEnter={() => handleMouseEnter(street.name,"street")}>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseEnter={() => handleMouseEnter(street.name, "street")}
+            >
               <img src={mapSvg} alt="map" id="iconImg" />
             </button>
-            <button className="icon" data-label="phone" onMouseEnter={() => handleMouseEnter(phone, "phone")}>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseEnter={() => handleMouseEnter(phone, "phone")}
+            >
               <img src={phoneSvg} alt="phone" id="iconImg" />
             </button>
-            <button className="icon" data-label="password" onMouseEnter={() => handleMouseEnter(login?.password,"password")}>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseEnter={() => handleMouseEnter(login?.password, "password")}
+            >
               <img src={padlockSvg} alt="lock" id="iconImg" />
             </button>
           </div>
@@ -121,21 +133,19 @@ const handleAdd =()=>{
             <thead>
               <tr className="head-tr">
                 <th className="th">Firstname</th>
-           
+
                 <th className="th">Email</th>
                 <th className="th">Phone</th>
                 <th className="th">Age</th>
               </tr>
-             
             </thead>
             <tbody>
-            {addUser.map(item => <Table {...item}/>)}
+              {addUser.map((item) => (
+                <Table {...item} />
+              ))}
             </tbody>
           </table>
         </div>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Footer />
       </div>
     </main>
   );
